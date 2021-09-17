a67532813af78f0b4dbb8528026110b81e1a4041
Colin Gourlay <gourlay.colin@abc.net.au>
2021-09-17 15:17:27 +1000

    Make text and background colours CSS custom properties that can be overridden.

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -18,8 +18,8 @@
 	:global(body) {
 		margin: 0;
 		min-height: var(--window-inner-height, 100vh);
-		background-color: rgba(0, 0, 0, 0);
-
+		background-color: var(--color-background, rgba(0, 0, 0, 0));
+		color: var(--color-text, rgba(0, 0, 0, 1));
 		display: flex;
 		flex-direction: column;
 		justify-content: center;
