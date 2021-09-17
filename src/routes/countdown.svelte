a975ad1bf3db10671c9360125eb6d41c460308a0
Colin Gourlay <gourlay.colin@abc.net.au>
2021-09-17 15:58:52 +1000

    Add separate countdown route which just shows the countdown (if one is running)

---
+++ src/routes/countdown.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+	import Countdown from '$components/Countdown/Countdown.svelte';
+	import countdown from '$stores/countdown';
+
+	let isFinished: boolean = false;
+
+	$: timeMS = $countdown ? +$countdown : null;
+</script>
+
+<article class:finished={isFinished}>
+	{#if timeMS}
+		<Countdown {timeMS} bind:isFinished />
+	{/if}
+</article>
+
+<style>
+	article {
+		transition: opacity 1.5s 0.25s, transform 2s ease-in-out;
+	}
+
+	.finished {
+		opacity: 0;
+		transform: scale(0.75);
+	}
+</style>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -2,7 +2,8 @@
 	<ul>
 		<li><a href="/controls">Controls</a></li>
 		<li><a href="/offline">Offline</a></li>
-		<li><a href="/standby">Standby (+ Countdown)</a></li>
+		<li><a href="/countdown">Countdown</a></li>
+		<li><a href="/standby">Standby (includes Countdown)</a></li>
 	</ul>
 </article>
 
