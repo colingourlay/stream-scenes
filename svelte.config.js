f23be9e02c57ab8f4731355da5e9ee6e5c9378c1
Colin Gourlay <gourlay.colin@abc.net.au>
2021-09-17 15:10:00 +1000

    Actually use the adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,6 @@
 import { resolve } from 'path';
 
-import adapter from '@sveltejs/adapter-static';
+import createStaticAdapter from '@sveltejs/adapter-static';
 import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -8,7 +8,7 @@ const config = {
 	preprocess: preprocess(),
 
 	kit: {
-		adapter,
+		adapter: createStaticAdapter(),
 		target: '#svelte',
 		vite: {
 			resolve: {
