b74da39e844246787fa7b73f775eee5d190f398f
Colin Gourlay <colin@colin-gourlay.com>
2022-09-20 23:56:30 +1000

    Fix layout custom properties

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -5,7 +5,7 @@
 
 <svelte:window bind:innerHeight={windowInnerHeight} bind:innerWidth={windowInnerWidth} />
 
-<main style={`--canvas-height:${windowInnerHeight}px;--canvas-width:${windowInnerHeight}px`}>
+<main style={`--canvas-height:${windowInnerHeight}px;--canvas-width:${windowInnerWidth}px`}>
 	<slot />
 </main>
 
@@ -21,7 +21,20 @@
 				var(--hue-primary) var(--saturation-primary) var(--lightness-primary) / var(--alpha-primary)
 			)
 		);
+		--hue-secondary: var(--theme-hue-secondary, var(--hue-primary));
+		--saturation-secondary: var(--theme-saturation-secondary, var(--saturation-primary));
+		--lightness-secondary: var(--theme-lightness-secondary, var(--lightness-primary));
+		--alpha-secondary: var(--theme-alpha-secondary, var(--alpha-primary));
+		--color-secondary: var(
+			--theme-color-secondary,
+			hsl(
+				var(--hue-secondary) var(--saturation-secondary) var(--lightness-primary) /
+					var(--alpha-secondary)
+			)
+		);
 		overflow: hidden;
+		background-color: var(--color-background, rgba(0, 0, 0, 0));
+		color: var(--color-text, rgba(0, 0, 0, 1));
 		font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell,
 			'Open Sans', 'Helvetica Neue', sans-serif;
 	}
@@ -29,8 +42,6 @@
 	:global(body) {
 		margin: 0;
 		min-height: var(--canvas-height, 100vh);
-		background-color: var(--color-background, rgba(0, 0, 0, 0));
-		color: var(--color-text, rgba(0, 0, 0, 1));
 		display: flex;
 		flex-direction: column;
 		justify-content: center;
