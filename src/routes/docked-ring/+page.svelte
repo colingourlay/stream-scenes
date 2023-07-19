33ec1c0cb00f65b0143723dec89f2c6e6d4c5c47
Colin Gourlay <colin@colin-gourlay.com>
2023-07-19 11:17:35 +1000

    Add /docked-ring/ route for returning a DockedRing page

---
+++ src/routes/docked-ring/+page.svelte
@@ -0,0 +1,17 @@
+<script>
+	import { browser } from '$app/environment';
+	import { page } from '$app/stores';
+	import DockedRing from '$lib/components/DockedRing/DockedRing.svelte';
+
+	/** @type {boolean} */
+	let isSecondary;
+	/**	@type {string | undefined} */
+	let position;
+
+	if (browser) {
+		isSecondary = $page.url.searchParams.has('secondary');
+		position = $page.url.searchParams.get('position') || undefined;
+	}
+</script>
+
+<DockedRing {position} {isSecondary} />
