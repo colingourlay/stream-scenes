9d9e5e842303968f78ab36dbc8067a0f61faff08
Colin Gourlay <colin@colin-gourlay.com>
2023-07-19 11:15:31 +1000

    Make Frame radius configurable (default to 0.2rem)

--- src/lib/components/Frame/Frame.svelte
+++ src/lib/components/Frame/Frame.svelte
@@ -1,10 +1,11 @@
 <script>
 	export let aspect = 16 / 9;
+	export let radius = '0.2rem';
 	export let isSecondary = false;
 </script>
 
 <figure
-	style={`--frame-aspect: ${aspect}; --frame-color: var(--color-${
+	style={`--frame-aspect: ${aspect}; --frame-radius: ${radius}; --frame-color: var(--color-${
 		isSecondary ? 'secondary' : 'primary'
 	});`}
 />
@@ -13,7 +14,7 @@
 	figure {
 		margin: 0;
 		outline: var(--frame-color) solid 0.2rem;
-		border-radius: 0.2rem;
+		border-radius: var(--frame-radius);
 		padding-bottom: calc(100% / var(--frame-aspect));
 		width: 100%;
 		height: 0;
