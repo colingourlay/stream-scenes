001316fff9c27d3d0d71549ef7074c3973f9217e
Colin Gourlay <colin@colin-gourlay.com>
2024-08-27 16:55:37 +1000

    Update Canva secondary colour and backdrop image gradient

--- src/lib/utils/preset.js
+++ src/lib/utils/preset.js
@@ -21,8 +21,7 @@ const isPreset = (key) => {
 
 /** @type {Record<string, string>} */
 export const PRESETS_IMAGES = /** @type {const} */ ({
-	// canva: `linear-gradient(120deg, #00c4cc 11%, #72be44 23%, #fd0 37%, #fbbf0a 40%, #f79d16 46%, #f5891d 50%, #f48120 53%, #ee402f 66%, #ed3e75 80%, #df3c83 81%, #bc35a7 84%, #a030c4 88%, #8d2dd8 91%, #812be4 94%, #7d2ae8 98%)`,
-	canva: `linear-gradient(120deg, var(--color-primary) 20%, #6420ff 50%, var(--color-secondary) 80%)`,
+	canva: `linear-gradient(to bottom right, var(--color-primary), var(--color-secondary))`,
 	synthwave: `linear-gradient(0deg, var(--color-primary) 50%, var(--color-secondary))`
 });
 
@@ -34,11 +33,11 @@ export const PRESETS_THEMES = /** @type {const} */ ({
 	},
 	canva: {
 		'hue-primary': '182.35',
-		'hue-secondary': '264.12',
+		'hue-secondary': '266.21',
 		'saturation-primary': '100%',
-		'saturation-secondary': '100%',
+		'saturation-secondary': '80.51%',
 		'lightness-primary': '40%',
-		'lightness-secondary': '61.96%'
+		'lightness-secondary': '53.73%'
 	},
 	monochrome: {
 		'saturation-primary': '0%',
