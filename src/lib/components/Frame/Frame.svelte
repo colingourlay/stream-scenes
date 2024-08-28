c58f47c6538bd96cf0a706197b0b1d5b9daed2c1
Colin Gourlay <colin@colin-gourlay.com>
2024-08-28 20:37:58 +1000

    Only measure `clientWidth`. Use rect for all frame types

--- src/lib/components/Frame/Frame.svelte
+++ src/lib/components/Frame/Frame.svelte
@@ -6,37 +6,29 @@
 	export let image = undefined;
 	export let isSecondary = false;
 
-	let wW = 0;
-	let oW = 0;
-	let oH = 0;
-	let cW = 0;
-	let cH = 0;
+	let viewportWidthPx = 0;
+	let contentWidthPx = 0;
 
-	$: thickness = `${thicknessVW}vw`;
-	$: radius = isCircle ? '50%' : `${thicknessVW * 2}vw`;
+	$: thicknessPx = Math.round((viewportWidthPx / 100) * thicknessVW);
+	$: contentHeightPx = Math.round(contentWidthPx / aspect);
+	$: contentRadiusPx = isCircle ? contentWidthPx / 2 : thicknessPx;
+	$: frameWidthPx = contentWidthPx + 2 * thicknessPx;
+	$: frameHeightPx = contentHeightPx + 2 * thicknessPx;
+	$: frameRadiusPx = isCircle ? frameWidthPx / 2 : thicknessPx * 2;
 	$: color = `var(--color-${isSecondary ? 'secondary' : 'primary'})`;
 	$: maskURL = `data:image/svg+xml,${encodeURIComponent(
-		`<svg xmlns="http://www.w3.org/2000/svg" width="${oW}" height="${oH}" viewBox="0 0 ${oW} ${oH}"><mask id="x"><rect x="0" y="0" width="${oW}" height="${oH}" fill="#fff" />${
-			isCircle
-				? `<circle cx="${oW / 2}" cy="${oH / 2}" r="${Math.min(cW, cH) / 2}" />`
-				: `<rect x="${(oW - cW) / 2}" y="${(oH - cH) / 2}" width="${cW}" height="${cH}" rx="${
-						(wW / 100) * thicknessVW
-				  }" />`
-		}</mask></svg>`
+		`<svg xmlns="http://www.w3.org/2000/svg"><mask id="x"><rect x="0" y="0" width="${frameWidthPx}" height="${frameHeightPx}" fill="#fff" /><rect x="${thicknessPx}" y="${thicknessPx}" width="${contentWidthPx}" height="${contentHeightPx}" rx="${contentRadiusPx}" /></mask></svg>`
 	)}#x`;
 </script>
 
-<svelte:window bind:innerWidth={wW} />
+<svelte:window bind:innerWidth={viewportWidthPx} />
 
 <figure
-	bind:offsetWidth={oW}
-	bind:offsetHeight={oH}
-	bind:clientWidth={cW}
-	bind:clientHeight={cH}
+	bind:clientWidth={contentWidthPx}
 	style={`
 		--frame-aspect: ${aspect};
-		--frame-thickness: ${thickness};
-		--frame-radius: ${radius};
+		--frame-thickness: ${thicknessPx}px;
+		--frame-radius: ${frameRadiusPx}px;
 		--frame-image: ${image || `linear-gradient(${color}, ${color})`};
 		--frame-mask: url(${maskURL});
 	`}
