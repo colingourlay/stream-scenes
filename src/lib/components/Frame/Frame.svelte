6f9e747cab2dd710df96c6e6bf3349dd1657323b
Colin Gourlay <colin@colin-gourlay.com>
2024-08-28 13:46:05 +1000

    Rewrite Frame & Ring
    - Make frame thickness configurable
    - Dynamically create center mask
    - Restore ability to use images/gradients
    - Add `isCircle` prop, for auto aspect & alternative mask

--- src/lib/components/Frame/Frame.svelte
+++ src/lib/components/Frame/Frame.svelte
@@ -1,19 +1,44 @@
 <script>
-	export let aspect = 16 / 9;
-	export let radius = '0.4rem';
+	export let thicknessVW = 0.5;
+	export let isCircle = false;
+	export let aspect = isCircle ? 1 : 16 / 9;
 	/** @type {string | undefined} */
 	export let image = undefined;
 	export let isSecondary = false;
 
+	let wW = 0;
+	let oW = 0;
+	let oH = 0;
+	let cW = 0;
+	let cH = 0;
+
+	$: thickness = `${thicknessVW}vw`;
+	$: radius = isCircle ? '50%' : `${thicknessVW * 2}vw`;
 	$: color = `var(--color-${isSecondary ? 'secondary' : 'primary'})`;
+	$: maskURL = `data:image/svg+xml,${encodeURIComponent(
+		`<svg xmlns="http://www.w3.org/2000/svg" width="${oW}" height="${oH}" viewBox="0 0 ${oW} ${oH}"><mask id="x"><rect x="0" y="0" width="${oW}" height="${oH}" fill="#fff" />${
+			isCircle
+				? `<circle cx="${oW / 2}" cy="${oH / 2}" r="${Math.min(cW, cH) / 2}" />`
+				: `<rect x="${(oW - cW) / 2}" y="${(oH - cH) / 2}" width="${cW}" height="${cH}" rx="${
+						(wW / 100) * thicknessVW
+				  }" />`
+		}</mask></svg>`
+	)}#x`;
 </script>
 
+<svelte:window bind:innerWidth={wW} />
+
 <figure
+	bind:offsetWidth={oW}
+	bind:offsetHeight={oH}
+	bind:clientWidth={cW}
+	bind:clientHeight={cH}
 	style={`
 		--frame-aspect: ${aspect};
+		--frame-thickness: ${thickness};
 		--frame-radius: ${radius};
-		--frame-image: linear-gradient(#000, #000), ${image || `linear-gradient(${color}, ${color})`};
-		--temp-frame-color: ${color};
+		--frame-image: ${image || `linear-gradient(${color}, ${color})`};
+		--frame-mask: url(${maskURL});
 	`}
 />
 
@@ -21,17 +46,15 @@
 	figure {
 		box-sizing: content-box;
 		aspect-ratio: var(--frame-aspect);
-		margin: -0.2rem;
-		/* border: transparent solid 0.2rem; */
+		margin: calc(var(--frame-thickness) * -1);
+		border: transparent solid var(--frame-thickness);
 		border-radius: var(--frame-radius);
 		width: 100%;
 		height: auto;
-		/* background-clip: padding-box, border-box; */
-		/* background-origin: padding-box, border-box; */
-		/* background-image: var(--frame-image); */
-
-		/* TEMPORARILY RESTORE COLOR-BASED BORDERS AS IMAGE-BASED ONES AREN'T MASKED */
-
-		border: var(--temp-frame-color) solid 0.2rem;
+		background-clip: border-box;
+		background-origin: border-box;
+		background-image: var(--frame-image);
+		-webkit-mask-image: var(--frame-mask);
+		mask-image: var(--frame-mask);
 	}
 </style>
