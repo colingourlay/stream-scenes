b3d061ec1bfd89c253990b00160e2ab54a4b1098
Colin Gourlay <colin@colin-gourlay.com>
2024-08-29 12:45:07 +1000

    Use resize observer to improve mearurement

--- src/lib/components/Frame/Frame.svelte
+++ src/lib/components/Frame/Frame.svelte
@@ -1,4 +1,6 @@
 <script>
+	import { resize } from 'svelte-resize-observer-action'
+
 	export let thicknessVW = 0.5;
 	export let isCircle = false;
 	export let aspect = isCircle ? 1 : 16 / 9;
@@ -6,25 +8,31 @@
 	export let image = undefined;
 	export let isSecondary = false;
 
-	let viewportWidthPx = 0;
-	let contentWidthPx = 0;
+	let thicknessPx = 0;
+	let frameRadiusPx = 0;
+	let maskURL = '';
 
-	$: thicknessPx = Math.round((viewportWidthPx / 100) * thicknessVW);
-	$: contentHeightPx = Math.round(contentWidthPx / aspect);
-	$: contentRadiusPx = isCircle ? contentWidthPx / 2 : thicknessPx;
-	$: frameWidthPx = contentWidthPx + 2 * thicknessPx;
-	$: frameHeightPx = contentHeightPx + 2 * thicknessPx;
-	$: frameRadiusPx = isCircle ? frameWidthPx / 2 : thicknessPx * 2;
+	/** @type {(entry: ResizeObserverEntry) => void} */
+	const onResize = (entry) => {
+		// thicknessPx = Math.round((window.innerWidth / 100) * thicknessVW);
+		thicknessPx = (window.innerWidth / 100) * thicknessVW;
+
+		const { width: contentWidthPx, height: contentHeightPx } = entry.contentRect;		
+		const contentRadiusPx = isCircle ? contentWidthPx / 2 : thicknessPx;
+		const frameWidthPx = contentWidthPx + 2 * thicknessPx;
+		const frameHeightPx = contentHeightPx + 2 * thicknessPx;
+		
+		frameRadiusPx = isCircle ? frameWidthPx / 2 : thicknessPx * 2;
+		maskURL = `data:image/svg+xml,${encodeURIComponent(
+			`<svg xmlns="http://www.w3.org/2000/svg"><mask id="x"><rect x="0" y="0" width="${frameWidthPx}" height="${frameHeightPx}" fill="#fff" /><rect x="${thicknessPx}" y="${thicknessPx}" width="${contentWidthPx}" height="${contentHeightPx}" rx="${contentRadiusPx}" /></mask></svg>`
+		)}#x`;
+	};
+
 	$: color = `var(--color-${isSecondary ? 'secondary' : 'primary'})`;
-	$: maskURL = `data:image/svg+xml,${encodeURIComponent(
-		`<svg xmlns="http://www.w3.org/2000/svg"><mask id="x"><rect x="0" y="0" width="${frameWidthPx}" height="${frameHeightPx}" fill="#fff" /><rect x="${thicknessPx}" y="${thicknessPx}" width="${contentWidthPx}" height="${contentHeightPx}" rx="${contentRadiusPx}" /></mask></svg>`
-	)}#x`;
 </script>
 
-<svelte:window bind:innerWidth={viewportWidthPx} />
-
 <figure
-	bind:clientWidth={contentWidthPx}
+	use:resize={onResize}
 	style={`
 		--frame-aspect: ${aspect};
 		--frame-thickness: ${thicknessPx}px;
@@ -46,7 +54,6 @@
 		background-clip: border-box;
 		background-origin: border-box;
 		background-image: var(--frame-image);
-		-webkit-mask-image: var(--frame-mask);
 		mask-image: var(--frame-mask);
 	}
 </style>
