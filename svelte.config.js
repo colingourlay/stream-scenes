56d28684855d5c439dfe1309b0cf5708676450d3
Colin Gourlay <colin@colin-gourlay.com>
2024-11-08 13:14:41 +1000

    Enforce runes mode

--- svelte.config.js
+++ svelte.config.js
@@ -2,6 +2,9 @@ import adapter from '@sveltejs/adapter-auto';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
+	compilerOptions: {
+		runes: true
+	},
 	kit: {
 		adapter: adapter()
 	}
